e64332b466ff1ac966a514471cb031a8a34859d4
Stephane Portron <stephane.portron@ledger.com>
2022-12-01 13:26:29 +0100

    FWEO-676: FAT - Crash upon Bluetooth pairing with iOS

--- src/os_io_task.c
+++ src/os_io_task.c
@@ -13,9 +13,9 @@ bolos_ux_asynch_callback_t G_io_asynch_ux_callback;
 
 #ifdef HAVE_BOLOS
 #ifdef HAVE_BLE
-# if (IO_SEPROXYHAL_BUFFER_SIZE_B != 192) && (defined(BOLOS_RELEASE) || !defined(DEBUG_VARIABLE_SPI_SIZE))
+# if (IO_SEPROXYHAL_BUFFER_SIZE_B < 192) && (defined(BOLOS_RELEASE) || !defined(DEBUG_VARIABLE_SPI_SIZE))
 #  error IO_SEPROXYHAL_BUFFER_SIZE_B size is invalid
-# endif // (IO_SEPROXYHAL_BUFFER_SIZE_B != 192) && (defined(BOLOS_RELEASE) || !defined(DEBUG_VARIABLE_SPI_SIZE))
+# endif // (IO_SEPROXYHAL_BUFFER_SIZE_B < 192) && (defined(BOLOS_RELEASE) || !defined(DEBUG_VARIABLE_SPI_SIZE))
 #else // !HAVE_BLE
 # if (IO_SEPROXYHAL_BUFFER_SIZE_B != 128) && (defined(BOLOS_RELEASE) || !defined(DEBUG_VARIABLE_SPI_SIZE))
 #  error IO_SEPROXYHAL_BUFFER_SIZE_B size is invalid
