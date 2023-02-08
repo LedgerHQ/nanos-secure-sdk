92335e215392892baed6a1852d2583be970b2541
Sarah GLINER <sarah.gliner@ledger.fr>
2023-02-08 17:59:33 +0100

    common_app/main.c: leave app_main on application side

--- common_app/main.c
+++ common_app/main.c
@@ -25,69 +25,6 @@
 uint8_t G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
 ux_state_t G_ux;
 bolos_ux_params_t G_ux_params;
-global_ctx_t G_context;
-
-/**
- * Handle APDU command received and send back APDU response using handlers.
- */
-void app_main() {
-    // Length of APDU command received in G_io_apdu_buffer
-    int input_len = 0;
-    // Structured APDU command
-    command_t cmd;
-
-    io_init();
-
-    // Reset context
-    explicit_bzero(&G_context, sizeof(G_context));
-
-    for (;;) {
-        BEGIN_TRY {
-            TRY {
-                // Reset structured APDU command
-                memset(&cmd, 0, sizeof(cmd));
-
-                // Receive command bytes in G_io_apdu_buffer
-                if ((input_len = io_recv_command()) < 0) {
-                    CLOSE_TRY;
-                    return;
-                }
-
-                // Parse APDU command from G_io_apdu_buffer
-                if (!apdu_parser(&cmd, G_io_apdu_buffer, input_len)) {
-                    PRINTF("=> /!\\ BAD LENGTH: %.*H\n", input_len, G_io_apdu_buffer);
-                    io_send_sw(SW_WRONG_DATA_LENGTH);
-                    CLOSE_TRY;
-                    continue;
-                }
-
-                PRINTF("=> CLA=%02X | INS=%02X | P1=%02X | P2=%02X | Lc=%02X | CData=%.*H\n",
-                       cmd.cla,
-                       cmd.ins,
-                       cmd.p1,
-                       cmd.p2,
-                       cmd.lc,
-                       cmd.lc,
-                       cmd.data);
-
-                // Dispatch structured APDU command to handler
-                if (apdu_dispatcher(&cmd) < 0) {
-                    CLOSE_TRY;
-                    return;
-                }
-            }
-            CATCH(EXCEPTION_IO_RESET) {
-                THROW(EXCEPTION_IO_RESET);
-            }
-            CATCH_OTHER(e) {
-                io_send_sw(e);
-            }
-            FINALLY {
-            }
-            END_TRY;
-        }
-    }
-}
 
 /**
  * Exit the application and go back to the dashboard.
@@ -124,8 +61,6 @@ WEAK __attribute__((section(".boot"))) int main() {
 #endif  // HAVE_BLE
                 USB_power(0);
                 USB_power(1);
-
-                ui_menu_main();
 
 #ifdef HAVE_BLE
                 BLE_power(0, NULL);
