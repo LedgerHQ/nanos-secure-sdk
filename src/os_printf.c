57188a63ab8ea56db636a90e82bf381b6fe12bca
Xavier Chapron <xavier.chapron@ledger.fr>
2023-03-27 18:47:11 +0200

    os_printf.c: Improve snprintf() handling of buffer of size 1
    (cherry picked from commit f2abce3e7cec4d6e6b11ae860750653e5f22cd9e)

--- src/os_printf.c
+++ src/os_printf.c
@@ -570,7 +570,7 @@ int snprintf(char * str, size_t str_size, const char * format, ...)
     //
     // Check the arguments.
     //
-    if(str == NULL ||str_size < 2) {
+    if(str == NULL ||str_size < 1) {
       return 0;
     }
 
@@ -578,6 +578,12 @@ int snprintf(char * str, size_t str_size, const char * format, ...)
     memset(str, 0, str_size);
     str_size--;
 
+    //
+    // Check if there is still space left for data in the buffer.
+    //
+    if(str_size < 1) {
+      return 0;
+    }
 
     //
     // Start the varargs processing.
