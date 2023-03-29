45345cadde7af32af94fe55a80c406fc5a29ae3b
Sarah GLINER <sarah.gliner@ledger.fr>
2023-03-29 18:06:19 +0200

    common_app: rename to standard_app

--- lib_standard_app/io.c
+++ lib_standard_app/io.c
@@ -63,7 +63,7 @@ WEAK uint8_t io_event(uint8_t channel) {
                   SEPROXYHAL_TAG_STATUS_EVENT_FLAG_USB_POWERED)) {
                 THROW(EXCEPTION_IO_RESET);
             }
-            /* fallthrough */
+        __attribute__((fallthrough));
         case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
 #ifdef HAVE_BAGL
             UX_DISPLAYED_EVENT({});
