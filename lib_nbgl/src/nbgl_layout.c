c82e685f60bfd1fcd875f758702327adbd183ce1
abonnaudet-ledger <arthur.bonnaudet@ledger.fr>
2023-05-12 17:28:44 +0200

    Fat: Use 4BPP for spinner text
    (cherry picked from commit af925f0d9dad8df0af3423182fdca8e90c25aa5b)

--- lib_nbgl/src/nbgl_layout.c
+++ lib_nbgl/src/nbgl_layout.c
@@ -1939,7 +1939,7 @@ int nbgl_layoutAddSpinner(nbgl_layout_t *layout, const char *text, bool fixed) {
   textArea->textColor = BLACK;
   textArea->text = PIC(text);
   textArea->textAlignment = CENTER;
-  textArea->fontId = BAGL_FONT_INTER_REGULAR_24px_1bpp;
+  textArea->fontId = BAGL_FONT_INTER_REGULAR_24px;
   textArea->alignmentMarginY = 36;
   textArea->alignTo = (nbgl_obj_t*)spinner;
   textArea->alignment = BOTTOM_MIDDLE;
