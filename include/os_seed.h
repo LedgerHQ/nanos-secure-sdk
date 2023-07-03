ed09015eb82f7b79cfbc463e43d362b300411575
yhql <victor_servant@hotmail.com>
2023-07-03 20:09:34 +0200

    fix: improper use of bzero
    (cherry picked from commit 26eb79a369c79c5cbca2b0fa9bc189d8b0571673)

--- include/os_seed.h
+++ include/os_seed.h
@@ -187,7 +187,7 @@ WARN_UNUSED_RESULT static inline cx_err_t os_derive_bip32_with_seed_no_throw(
 
             // Make sure the caller doesn't use uninitialized data in case
             // the return code is not checked.
-            explicit_bzero(&raw_privkey, 64);
+            explicit_bzero(raw_privkey, 64);
         }
         FINALLY {
         }
@@ -276,7 +276,7 @@ WARN_UNUSED_RESULT static inline cx_err_t os_derive_eip2333_no_throw(
 
             // Make sure the caller doesn't use uninitialized data in case
             // the return code is not checked.
-            explicit_bzero(&raw_privkey, 64);
+            explicit_bzero(raw_privkey, 64);
         }
         FINALLY {
         }
