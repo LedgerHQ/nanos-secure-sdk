35be2157e4b3bbd2c466a0bf9ee9d95a402ec9a6
Philippe Bonnaz <philippe@ledger.fr>
2023-08-02 17:20:19 +0200

    Add endorsment revoke prototypes in os_endorsement.h

--- include/os_endorsement.h
+++ include/os_endorsement.h
@@ -7,6 +7,13 @@
 /* -                         ENDORSEMENT FEATURE                         - */
 /* ----------------------------------------------------------------------- */
 
+typedef enum endorsement_revoke_id_e {
+    ENDORSEMENT_REVOKE_ID_ALL   = 0,
+    ENDORSEMENT_REVOKE_ID_SLOT1 = 1,
+    ENDORSEMENT_REVOKE_ID_SLOT2 = 2,
+    ENDORSEMENT_REVOKE_ID_LAST  = 3,
+} endorsement_revoke_id_t;
+
 #define ENDORSEMENT_MAX_ASN1_LENGTH                     (1 + 1 + 2 * (1 + 1 + 33))
 
 SYSCALL unsigned int os_endorsement_get_code_hash(unsigned char* buffer PLENGTH(32));
@@ -17,3 +24,7 @@ SYSCALL unsigned int os_endorsement_key1_sign_data(unsigned char* src PLENGTH(sr
 SYSCALL unsigned int os_endorsement_key2_derive_sign_data(unsigned char* src PLENGTH(srcLength), unsigned int srcLength, unsigned char* signature PLENGTH(ENDORSEMENT_MAX_ASN1_LENGTH));
 
 SYSCALL unsigned int os_endorsement_get_metadata(unsigned char index, unsigned char* buffer PLENGTH(8));
+
+SYSCALL void os_endorsement_revoke_slot1(void);
+SYSCALL void os_endorsement_revoke_slot2(void);
+SYSCALL void os_endorsement_revoke_all(void);
