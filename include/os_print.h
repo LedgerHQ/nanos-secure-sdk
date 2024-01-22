e24bb5d587b094eadfc15f1ace185525a5f9bc32
Xavier Chapron <xavier.chapron@ledger.fr>
2024-01-22 11:47:44 +0100

    os_print.h: Move PRINTF declaration in dedicated header
    (cherry picked from commit d345ef6829209e963ff57f2eece5b17a78d19c4e)

--- include/os_print.h
+++ include/os_print.h
@@ -4,3 +4,10 @@
 
 // avoid typing the size each time
 #define SPRINTF(strbuf, ...) snprintf(strbuf, sizeof(strbuf), __VA_ARGS__)
+
+#ifdef HAVE_PRINTF
+void screen_printf(const char *format, ...);
+void mcu_usb_printf(const char *format, ...);
+#else  // !HAVE_PRINTF
+#define PRINTF(...)
+#endif  // !HAVE_PRINTF
