9460439030d2ea5f773d9a3f6200ac2bd03fa13b
Baptistin BOILOT <baptistin.boilot@ledger.fr>
2024-02-20 17:42:03 +0100

    fix: read out of bounds in bip32_path_read

--- fuzzing/fuzzer_base58.c
+++ fuzzing/fuzzer_base58.c
@@ -10,6 +10,6 @@
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     uint8_t out[200];
-    base58_decode((const char *) data, size, out, 8192);
+    base58_decode((const char *) data, size, out, 200);
     return 0;
 }
